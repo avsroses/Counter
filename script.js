b99d2424f587743654ff288ac81fdc73885400eb
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2024-10-31 10:09:13 +0000

    js

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+
 async function fetchCounter() {
     try {
+        // define URL
+        const getUrl = "https://api.counterapi.dev/v1/halloween/CTL"
+
+        // fetch response
+        const response = await fetch(getUrl);
+
+        // check response okay
+        if(!response.ok) {
+            throw new Error("Response status: ", response.status);
+        }
+
+        // get json
+        const json = await response.json();
         
     } catch(error) {
         console.error(error);
